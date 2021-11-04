622eca7fec06db6f166fc4c6c9dc22a3dbd5f768
grongworks <christian.koethe@grong.works>
2021-11-04 14:04:34 +0100

    added first version of package

---
+++ index.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const twig = require("twig");
+
+module.exports = class {
+    constructor () {
+        this
+            .initBrowser()
+            .initTwig();
+
+        this.disableTwigCache();
+    }
+
+    initTwig () {
+        this.twig = twig;
+        return this;
+    }
+
+    initBrowser () {
+        this.browser = puppeteer.launch({
+            headless: true,
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        });
+
+        return this;
+    }
+
+    enableTwigCache () {
+        this.twig.cache(true);
+        return this;
+    }
+
+
+
+    render (templatePath, data) {
+        if (!fs.existsSync(templatePath)) throw new Error("template_file_not_exists");
+
+        const renderedHtml = this.twig.renderFile(templatePath, data, (error, html) => {
+            if (error !== null) throw new Error("error_rendering_template");
+            debugger;
+        });
+    }
+}
